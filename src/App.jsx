112b426e2566731a20f76fc3472d95c6c405aa38
eibz <atsmultanen@gmail.com>
2021-06-26 18:34:07 +0100

    Adding env & mainbuilder files + updates to linter

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainBuilder from './components/MainBuilder';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Hi
-        </a>
-      </header>
+      <MainBuilder />
     </div>
   );
 }
