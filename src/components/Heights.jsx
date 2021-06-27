acac210ea887f8dd816a968f04629a997c5047c7
AtsMultanen <ats.multanen@dm-companies.com>
2021-06-27 15:00:53 +0100

    Trying to get heights to show depending on chosen position

--- src/components/Heights.jsx
+++ src/components/Heights.jsx
@@ -1,16 +1,29 @@
 import React, { useContext } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Slider from '@material-ui/core/Slider';
 import Typography from '@material-ui/core/Typography';
 import { DataContext } from './dataContext';
 import availableHeights from '../db/heightsObject';
 
-function valueLabelFormat(value) {
-  return availableHeights[value].label;
-}
+const useStyles = makeStyles(() => ({
+  formControl: {
+    margin: '50px auto',
+    maxWidth: 300,
+  },
+}));
 
 export default function Heights() {
   const appData = useContext(DataContext);
   const [value, setValue] = React.useState(1);
+  const classes = useStyles();
+
+  function valueLabelFormat(number) {
+    console.log(`chosen position: ${appData.position}`);
+    console.log(`positions available for chosen height: ${availableHeights[value].positions}`);
+    console.log(availableHeights[value].positions.includes(appData.position));
+    // const nanoIndexes = availableHeights.flatMap((car, i) => car === 'C' ? i : []);
+    return availableHeights[number].label;
+  }
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -24,15 +37,16 @@ export default function Heights() {
       <Slider
         disabled={appData.heightDisabled}
         value={value}
-        min={0}
+        min={5}
         step={1}
-        max={22}
+        max={15} // max 22
         marks={availableHeights}
         getAriaValueText={valueLabelFormat}
         valueLabelFormat={valueLabelFormat}
         onChange={handleChange}
         valueLabelDisplay="auto"
         aria-labelledby="non-linear-slider"
+        className={classes.formControl}
       />
       <div>{appData.position}</div>
     </div>
--- src/db/heightsObject.js
+++ src/db/heightsObject.js
@@ -2,95 +2,123 @@ const availableHeights = [
   {
     value: 0,
     label: '5\'5"',
+    positions: ['PG'],
   },
   {
     value: 1,
     label: '5\'6"',
+    positions: ['PG'],
   },
   {
     value: 2,
     label: '5\'7"',
+    positions: ['PG'],
   },
   {
     value: 3,
     label: '5\'8"',
+    positions: ['PG'],
   },
   {
     value: 4,
     label: '5\'9"',
+    positions: ['PG'],
   },
   {
     value: 5,
     label: '5\'10"',
+    positions: ['PG'],
   },
   {
     value: 6,
     label: '5\'11"',
+    positions: ['PG'],
   },
   {
     value: 7,
     label: '6\'0"',
+    positions: ['PG', 'SG'],
   },
   {
     value: 8,
     label: '6\'1"',
+    positions: ['PG', 'SG'],
   },
   {
     value: 9,
     label: '6\'2"',
+    positions: ['PG', 'SG'],
   },
   {
     value: 10,
     label: '6\'3"',
+    positions: ['PG', 'SG'],
   },
   {
     value: 11,
     label: '6\'4"',
+    positions: ['PG', 'SG'],
   },
   {
     value: 12,
     label: '6\'5"',
+    positions: ['PG', 'SG', 'SF'],
   },
   {
     value: 13,
     label: '6\'6"',
+    positions: ['PG', 'SG', 'SF'],
   },
   {
     value: 14,
     label: '6\'7"',
+    positions: ['PG', 'SG', 'SF', 'PF'],
   },
   {
     value: 15,
     label: '6\'8"',
+    positions: ['PG', 'SG', 'SF', 'PF'],
   },
   {
     value: 16,
     label: '6\'9"',
+    positions: ['SG', 'SF', 'PF', 'C'],
   },
   {
     value: 17,
     label: '6\'10"',
+    positions: ['SF', 'PF', 'C'],
   },
   {
     value: 18,
     label: '6\'11"',
+    positions: ['PF', 'C'],
   },
   {
     value: 19,
     label: '7\'0"',
+    positions: ['PF', 'C'],
   },
   {
     value: 20,
     label: '7\'1"',
+    positions: ['C'],
   },
   {
     value: 21,
     label: '7\'2"',
+    positions: ['C'],
   },
   {
     value: 22,
     label: '7\'3"',
+    positions: ['C'],
   },
 ];
+
+const heightsPerPosition = {
+  C: ['7\'3"', '7\'2"', '7\'1"', '7\'0"', '6\'11"', '6\'10"'],
+
+};
 
 export default availableHeights;
